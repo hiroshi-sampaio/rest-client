d7b51409332b66648ec74d1bb1b3b5e3da99cd6e
Hiroshi Sampaio <17440771+hiroshi-sampaio@users.noreply.github.com>
2021-06-14 17:21:53 -0300

    Set discovery client Feign reference hostname

--- src/main/java/com/sampaio/hiroshi/restclient/RestClientApplication.java
+++ src/main/java/com/sampaio/hiroshi/restclient/RestClientApplication.java
@@ -4,19 +4,26 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 enum BananaVariety {
-    Red, LadyFinger, BlueJava, Manzano, Burro, Plantain;
+    Red, LadyFinger, BlueJava, Manzano, Burro, Plantain
 }
 
-@FeignClient(name = "feign-client", url = "${com.sampaio.hiroshi.rest-server.url}")
+@FeignClient("rest-server")
 interface BananaVarietyClient {
     @GetMapping(path = "/getBananaVariety", produces = "application/json")
     ResponseEntity<Banana> getBanana(@RequestParam final String variety);
@@ -56,4 +63,20 @@ public class RestClientApplication {
 class Banana {
     private BananaVariety variety;
     private String whoAmI;
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    private final DiscoveryClient discoveryClient;
+
+    ServiceInstanceRestController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @GetMapping("/service-instances/{applicationName}")
+    public ResponseEntity<List<ServiceInstance>> getServiceInstance(@PathVariable final String applicationName) {
+        return new ResponseEntity<>(discoveryClient.getInstances(applicationName), HttpStatus.OK);
+    }
+
 }
